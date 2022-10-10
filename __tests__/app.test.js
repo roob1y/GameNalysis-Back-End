95dd26e2118775e94b947bb651835a8088bcf72c
roob1y <robwork122@gmail.com>
2022-10-10 17:09:04 +0100

    test is made for GET /api/reviews/:review_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -37,3 +37,23 @@ describe("GET /api/categories", () => {
       });
   });
 });
+describe("GET /api/reviews/:review_id", () => {
+  test("200: should return a list of categories ", () => {
+    return request(app)
+      .get("/api/reviews/:review_id")
+      .expect(200)
+      .then(({ review }) => {
+          expect(review).toMatchObject({
+            review_id: expect.any(Number),
+            title: expect.any(String),
+            review_body: expect.any(String),
+            designer: expect.any(String),
+            review_img_url: expect.any(String),
+            votes: expect.any(Number),
+            category: expect.any(String),
+            owner: expect.any(String),
+            created_at: expect.any(String)
+          });
+        });
+      });
+  });
