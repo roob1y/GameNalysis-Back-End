d1cb36bdde600994ea8275d4098140f38ea7ff60
roob1y <robwork122@gmail.com>
2022-12-05 16:53:06 +0000

    combined routes with same query

--- routes/comments-router.js
+++ routes/comments-router.js
@@ -1,8 +1,13 @@
 const commentsRouter = require("express").Router();
 
-const { postVoteByCommentId, deleteByCommentId } = require("../controllers/comments.controllers");
+const {
+  postVoteByCommentId,
+  deleteByCommentId,
+} = require("../controllers/comments.controllers");
 
-commentsRouter.patch("/:comment_id", postVoteByCommentId);
-commentsRouter.delete("/:comment_id", deleteByCommentId);
+commentsRouter
+  .route("/:comment_id")
+  .patch(postVoteByCommentId)
+  .delete(deleteByCommentId);
 
 module.exports = commentsRouter;
